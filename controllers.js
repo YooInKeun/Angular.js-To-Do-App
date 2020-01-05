2bde2a41c8444631beea185c24f745be3e9dc0f7
YooInKeun <keun0390@naver.com>
2020-01-05 17:31:11 +0900

    [UPDATE] ver2 UPDATE 기능

--- controllers.js
+++ controllers.js
@@ -2,14 +2,17 @@ angular.module('todo').controller('TodoCtrl', function($scope) {
 
   $scope.todos = [
     {
+      id: 1,
       title: 'Spring Boot Rest API',
       content: '백엔드 간단한 CRUD 기능',
     },
     {
+      id: 4,
       title: 'Angular.js 프론트',
       content: '프론트 간단한 CRUD 기능',
     },
     {
+      id: 3,
       title: '운동하기',
       content: '유산소 운동',
     }
@@ -17,7 +20,7 @@ angular.module('todo').controller('TodoCtrl', function($scope) {
 
   $scope.remove = function(todo) {
     var idx = $scope.todos.findIndex(function (item) {
-      return item.title === todo.title;
+      return item.id === todo.id;
     })
 
     if (idx > -1) {
@@ -36,6 +39,12 @@ angular.module('todo').controller('TodoCtrl', function($scope) {
     $scope.newTodoContent = "";
   }
 
-  $scope.update = function () {
+  $scope.update = function (todo, updatedTodoTitle, updatedTodoContent) {
+    var idx = $scope.todos.findIndex(function (item) {
+      return item.id === todo.id;
+    });
+
+    $scope.todos[idx].title = updatedTodoTitle;
+    $scope.todos[idx].content = updatedTodoContent;
   };
 });
